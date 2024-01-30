ecd50800b85f6b1c8f6e188e876e16a55072dfff
Pedro Giampietro <pedrogiampietro@hotmail.com>
2024-01-30 19:43:55 -0300

    add: readme

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -15,7 +15,7 @@ export default async function Home() {
 			<Header />
 
 			<div className='px-5 pt-5'>
-				<h2 className='text-xl font-bold'>Olá, Miguel!</h2>
+				<h2 className='text-xl font-bold'>Olá, Pedro!</h2>
 				<p className='capitalize text-sm'>
 					{format(new Date(), "EEEE',' dd 'de' MMMM", {
 						locale: ptBR,
@@ -40,7 +40,7 @@ export default async function Home() {
 				</h2>
 
 				<div className='flex px-5 gap-4 overflow-x-auto [&::-webkit-scrollbar]:hidden'>
-					{barbershops.map((barbershop) => (
+					{barbershops.map((barbershop: any) => (
 						<BarbershopItem key={barbershop.id} barbershop={barbershop} />
 					))}
 				</div>
@@ -52,7 +52,7 @@ export default async function Home() {
 				</h2>
 
 				<div className='flex px-5 gap-4 overflow-x-auto [&::-webkit-scrollbar]:hidden'>
-					{barbershops.map((barbershop) => (
+					{barbershops.map((barbershop: any) => (
 						<BarbershopItem key={barbershop.id} barbershop={barbershop} />
 					))}
 				</div>
